f9fa0e81760faa4eed7e30a6abdaa81e84f957ea
Tejas Sandeep Khurd <officialtejaspro@gmail.com>
2025-07-20 18:54:20 +0530

    Various Pattern
    Solving various pattern printing questions to build and strengthen programming logic.

---
+++ Pattern Printing/Ulta_Num_Alphabet_Triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int r;
+    printf("Enter a rows: ");
+    scanf("%d", &r);
+
+    for(int i = 1; i <= r; i++) {
+        for(int j = 1; j <=r+1-i; j++) {
+            if(i%2 == 0) printf("%c ",j+64);
+            else printf("%d ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
