1bdc6eefd6e9d320d7f3f79cf04abcc13f1d5c8c
Tejas Sandeep Khurd <officialtejaspro@gmail.com>
2025-07-20 13:36:15 +0530

    Various Patterns
    Solving various pattern printing questions to build and strengthen programming logic.

---
+++ Pattern Printing/Ulta_Even_Triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int r;
+    printf("Enter a rows: ");
+    scanf("%d", &r);
+
+    for(int i = 1; i <= r; i++) {
+        int a =2;
+        for(int j = 1; j <= r+1-i; j++) {
+            printf("%d ",a);
+            a+=2;
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Pattern Printing/Ulta_Triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int r;
+    printf("Enter a rows: ");
+    scanf("%d", &r);
+
+    for(int i = 1; i <= r; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("%c ",j+64);
+        }
+        printf("\n");
+    }
+    return 0;
+}
